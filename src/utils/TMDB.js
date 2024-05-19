3d1ecbd8a61ce885314ee2cd9b6d65d9d4a38b89
madhav019 <madhavchawla19@gmail.com>
2024-05-19 11:20:28 +0530

    files deleted

--- src/utils/TMDB.js
+++
@@ -1,8 +0,0 @@
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MGYxYWYyM2Y4ZTg3NWU3OTIzODY5Yzg5NDUxOGQ0MSIsInN1YiI6IjY2NDg3ZGQyZGVhZWI3ZjhhZjBhYmNjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sJqRBqKZf3YVVF8jLK3XiGCj3bCf89xSqrw_zJzy4Ws",
-  },
-};
